a378f60480b3bd1249d5747cdaa208863d74f672
Gabi Villalonga Simon <gvillalongasimon@cloudflare.com>
2025-04-03 14:53:04 -0500

    health

--- load-balancer/container/main.go
+++ load-balancer/container/main.go
@@ -5,6 +5,9 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
@@ -16,6 +19,34 @@ func handler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	c := make(chan os.Signal, 10)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	terminate := false
+	go func() {
+		for range c {
+			if terminate {
+				os.Exit(0)
+				continue
+			}
+
+			terminate = true
+			go func() {
+				time.Sleep(time.Minute)
+				os.Exit(0)
+			}()
+		}
+	}()
+
+	http.HandleFunc("/_health", func(w http.ResponseWriter, r *http.Request) {
+		if terminate {
+			w.WriteHeader(400)
+			w.Write([]byte("draining"))
+			return
+		}
+
+		w.Write([]byte("ok"))
+	})
+
 	http.HandleFunc("/", handler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
