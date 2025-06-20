46fe598fd875900d42cdebef482699310ece861e
Gabi Villalonga Simon <gvillalongasimon@cloudflare.com>
2025-06-20 10:19:02 -0500

    additional fixes

--- marimo/src/index.ts
+++ marimo/src/index.ts
@@ -23,45 +23,12 @@ export class Container extends DurableObject<Env> {
 		super(ctx, env);
 		this.container = ctx.container!;
 		this.blockConcurrencyRetry(async () => {
-			await this.initWebsocket();
+			await this.init();
 		});
 	}
 
-	conn?: WebSocket;
-	async initWebsocket() {
+	async init() {
 		if (!this.container.running) this.container.start();
-
-		const res = await this.container.getTcpPort(8080).fetch(new Request('http://container/ws', { headers: { Upgrade: 'websocket' } }));
-		if (res.webSocket === null) throw new Error('websocket server is faulty');
-
-		// Accept the websocket and listen to messages
-		res.webSocket.accept();
-		res.webSocket.addEventListener('message', (msg) => {
-			if (this.resolveResolve !== undefined)
-				this.resolveResolve(typeof msg.data === 'string' ? msg.data : new TextDecoder().decode(msg.data));
-		});
-
-		res.webSocket.addEventListener('close', () => {
-			this.ctx.abort();
-		});
-
-		this.conn = res.webSocket;
-	}
-
-	promise?: Promise<string>;
-	resolveResolve?: (s: string) => void;
-	async send(message: string) {
-		// add a promise to the class and send a message
-		this.promise = new Promise((res) => {
-			this.resolveResolve = res;
-		});
-
-		this.conn?.send(message);
-	}
-
-	async receive(): Promise<string> {
-		if (this.promise !== undefined) return await this.promise;
-		return '<no message>';
 	}
 
 	async fetch(req: Request) {
@@ -74,12 +41,6 @@ export default {
 	async fetch(request, env): Promise<Response> {
 		const id: DurableObjectId = env.CONTAINER.idFromName('foo');
 		const stub = env.CONTAINER.get(id);
-		if (request.method !== 'POST') {
-			return await stub.fetch(request);
-		}
-
-		await stub.send('we sent: ' + (await request.text()));
-		const message = await stub.receive();
-		return new Response(message);
+		return await stub.fetch(request);
 	},
 } satisfies ExportedHandler<Env>;
